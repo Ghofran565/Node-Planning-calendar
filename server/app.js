f0b69ed8afbf4b830349f9d20442f7a081cf7e8d
Mohammad Hossein <mym25340mmm@gmail.com>
2024-10-20 15:27:08 +0330

    Updated & Completed: app.js
    app.js has been completed and all endpoints were worked

--- server/app.js
+++ server/app.js
@@ -8,7 +8,12 @@ import catchError from './Utils/catchError.js';
 import HandleError from './Utils/handleError.js';
 
 ///* custom imports *\\\
+import adminRouter from './Routes/admin.js';
+import authRouter from './Routes/auth.js';
+import coursesRouter from './Routes/courses.js';
+import reportsRouter from './Routes/reports.js';
 import uploadRouter from './Routes/upload.js';
+import userRouter from './Routes/user.js';
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -22,7 +27,12 @@ app.use(morgan('dev'));
 app.use(cors()); //TODO: fill with front local path
 
 ///* custom app uses *\\\
+app.use('/api/admin', adminRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/courses', coursesRouter);
+app.use('/api/reports', reportsRouter);
 app.use('/api/upload', uploadRouter);
+app.use('/api/user', userRouter);
 
 app.use('*', (req, res, next) => {
 	return next(new HandleError('Invalid route', 404));
