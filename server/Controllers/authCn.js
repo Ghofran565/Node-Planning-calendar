c298b4c768ed3ff96a99db1d6fa902f9c735606b
Mohammad Hossein <mym25340mmm@gmail.com>
2024-10-20 14:43:22 +0330

    Updated & fixed: authCn.js
    the auth Cn was just working for a normal user not both admins and users so this bug has been fixed

--- server/Controllers/authCn.js
+++ server/Controllers/authCn.js
@@ -4,6 +4,7 @@ import catchAsync from '../Utils/catchAsync.js';
 import HandleError from '../Utils/handleError.js';
 import { sendEmailCode, verifyEmailCode } from '../Utils/emailHandler.js';
 import User from '../Models/userMd.js';
+import Admin from './../Models/adminMd.js';
 
 const nationalIdRegex = /^[0-9]{10}$/g;
 const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
@@ -14,12 +15,14 @@ const generateToken = (user, additionalPayload = {}) => {
 	const payload = {
 		id: user._id,
 		email: user.email,
+		role: user.role,
 		...additionalPayload,
 	};
 	return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '5d' });
 };
 
 export const auth = catchAsync(async (req, res, next) => {
+	let person;
 	const { nationalId, password } = req.body;
 	if (!nationalIdRegex.test(nationalId)) {
 		return next(new HandleError('Invalid NationalId format.', 400));
@@ -29,16 +32,23 @@ export const auth = catchAsync(async (req, res, next) => {
 	}
 
 	const user = await User.findOne({ nationalId });
+	const admin = await Admin.findOne({ nationalId });
 
-	if (!user) {
-		return next(new HandleError('No users found by this NationalId.', 400));
-	} else if (!bcryptjs.compareSync(password, user.password)) {
+	if (user) {
+		person = user;
+	} else if (admin) {
+		person = admin;
+	} else {
 		return next(
-			new HandleError('Wrong password. Please try again.', 401)
+			new HandleError('No users or admins found by this NationalId.', 404)
 		);
 	}
 
-	const token = generateToken(user);
+	if (!bcryptjs.compareSync(password, person.password)) {
+		return next(new HandleError('Wrong password.', 401));
+	}
+
+	const token = generateToken(person);
 
 	return res.status(200).json({
 		success: true,
@@ -50,22 +60,35 @@ export const auth = catchAsync(async (req, res, next) => {
 });
 
 export const forgetPassword = catchAsync(async (req, res, next) => {
+	let person;
 	const { nationalId } = req?.body;
+
 	if (!nationalIdRegex.test(nationalId)) {
 		return next(new HandleError('Invalid NationalId format.', 400));
 	}
+
 	const user = await User.findOne({ nationalId });
-	if (!user) {
-		return next(new HandleError('No users found by this NationalId.', 404));
+	const admin = await Admin.findOne({ nationalId });
+
+	if (user) {
+		person = user;
+	} else if (admin) {
+		person = admin;
+	} else {
+		return next(
+			new HandleError('No users or admins found by this NationalId.', 404)
+		);
 	}
-	sendEmailCode(user.email);
+
+	sendEmailCode(person.email);
 	return res.status(200).json({
 		success: true,
-		message: `We have sent the code to ${user.email}.`,
+		message: `We have sent the code to ${person.email}.`,
 	});
 });
 
 export const checkForgetPassword = catchAsync(async (req, res, next) => {
+	let person;
 	const { email, code } = req?.body;
 	if (!emailRegex.test(email)) {
 		return next(new HandleError('Invalid email format.', 400));
@@ -73,15 +96,29 @@ export const checkForgetPassword = catchAsync(async (req, res, next) => {
 	if (!verificationCodeRegex.test(code)) {
 		return next(new HandleError('Invalid verification code format.', 400));
 	}
+
 	const user = await User.findOne({ email });
-	if (!user) {
-		return next(new HandleError('User not found. Please sign up first.', 404));
+	const admin = await Admin.findOne({ email });
+
+	if (user) {
+		person = user;
+	} else if (admin) {
+		person = admin;
+	} else {
+		return next(
+			new HandleError('No users or admins found by this Email.', 404)
+		);
 	}
+
 	const verificationResult = verifyEmailCode(email, code);
 	if (!verificationResult.authorized) {
 		return next(new HandleError('Invalid verification code.', 401));
 	}
-	const token = generateToken(user, { changePassword: true });
+
+	token = generateToken(person, {
+		changePassword: true,
+	});
+
 	return res.status(200).json({
 		success: true,
 		data: { token },
@@ -91,6 +128,7 @@ export const checkForgetPassword = catchAsync(async (req, res, next) => {
 });
 
 export const changePassword = catchAsync(async (req, res, next) => {
+	let person;
 	const { id: bodyId, password } = req?.body;
 	const { id, changePassword } = req.decodedToken;
 
@@ -116,17 +154,31 @@ export const changePassword = catchAsync(async (req, res, next) => {
 	}
 
 	const hashedPassword = bcryptjs.hashSync(password, 10);
+
 	const user = await User.findByIdAndUpdate(
 		id,
 		{ password: hashedPassword },
 		{ new: true, runValidators: true }
 	);
 
-	if (!user) {
-		return next(new HandleError('User not found. Please try again.', 404));
+	const admin = await Admin.findByIdAndUpdate(
+		id,
+		{ password: hashedPassword },
+		{ new: true, runValidators: true }
+	);
+
+	if (user) {
+		person = user;
+	} else if (admin) {
+		person = admin;
+	} else {
+		return next(
+			new HandleError('No users or admins found by this NationalId.', 404)
+		);
 	}
 
-	const newToken = generateToken(user);
+
+	const newToken = generateToken(person);
 
 	return res.status(200).json({
 		success: true,
@@ -136,6 +188,3 @@ export const changePassword = catchAsync(async (req, res, next) => {
 		message: 'Password changed successfully.',
 	});
 });
-
-
-//TODO User auth and admin aurh are diffrent
